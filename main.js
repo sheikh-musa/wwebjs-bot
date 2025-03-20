c95fa6cbcbcab725819f9705431f681fc5ee49cb
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2025-03-20 16:11:56 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+// Create a new client instance
+const client = new Client();
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Client is ready!');
+});
+
+// When the client received QR-Code
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+});
+
+// Start your client
+client.initialize();
+
+// Listening to all incoming messages
+client.on('message_create', message => {
+	console.log(message.body);
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+        message.reply('pinging the pong');
+	}
+});
